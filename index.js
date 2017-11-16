60d60a536878000feff63bbdebe8c8b1da663a3c
nocheinarbeiter <nocheinarbeiter@protonmail.com>
2017-11-16 16:41:56 +0300

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+var pathUtil = require('path');
+var createTransformStream = require('./lib/create-transform-stream');
+var generateFactoryModule = require('./lib/generate-markup-module');
+
+
+function gulpReactMarkup(options) {
+    var fileNameSuffix = (options && options.suffix) || '';
+    return createTransformStream('gulp-react-markup', ({base, path, contents}, cb) => {
+        generateFactoryModule(extractName(path), contents)
+            .then(moduleSource => cb(null, {
+                base,
+                path: changePathExtension(path, fileNameSuffix + '.ts'),
+                contents: new Buffer(moduleSource)
+            }))
+            .catch(error => cb(error))
+    });
+}
+module.exports = gulpReactMarkup;
+
+function changePathExtension(path, ext) {
+    var {dir, name} = pathUtil.parse(path);
+    name = trimExtension(name);
+    return pathUtil.format({dir, name, ext})
+}
+
+function extractName(path) {
+    return trimExtension(pathUtil.basename(path));
+}
+
+function trimExtension(basename) {
+    return basename.replace(/(\..*)/, '');
+}
